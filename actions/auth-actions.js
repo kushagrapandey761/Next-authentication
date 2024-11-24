8d08f6fb6a3c17d355782a9b203de00bada6e209
Kushagra Pandey <kushagra.15554@gmail.com>
2024-11-24 22:53:36 +0530

    Extracted and validated user input

---
+++ actions/auth-actions.js
@@ -0,0 +1,19 @@
+"use server";
+export async function signup(prevState, formData) {
+  const email = formData.get("email");
+  const password = formData.get("password");
+  
+  let errors = {};
+  if (!email.includes("@")) {
+    errors.email = "Please enter a valid email address";
+  }
+  if (password.trim().length < 8) {
+    errors.password = "Passwords must be atleast 8 characters long";
+  }
+  console.log(errors);
+  if (Object.keys(errors).length > 0) {
+    return {
+      errors,
+    };
+  }
+}
